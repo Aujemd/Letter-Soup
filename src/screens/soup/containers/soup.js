10ef8e5782c4e50e54501396ac1f9967b76a83ab
Johan Mora <joenmodu99@gmail.com>
2020-01-28 14:33:38 -0400

    Label de las palabras principales creado sin ser responsive a niveles

--- src/screens/soup/containers/soup.js
+++ src/screens/soup/containers/soup.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react'
-import {View} from 'react-native'
+import {View , StyleSheet} from 'react-native'
 import Board from '../components/board'
+import WordsLabel from '../components/wordsLabel'
 
 class Soup extends Component {
 
     render(){
         return(
-            <Board></Board>
+            <View style = {Styles.container}>
+                <WordsLabel words = {['SAL', 'CARNE', 'POLLO', 'MANI', 'PAN', 'PEZ', 'ARROZ', 'UVA' , 'HARINA', 'PAPA', 'PERA', 'PASTA', 'MAIZ', 'DONA', 'LECHE', 'AJO']}/>
+                <Board></Board>
+            </View>
         )
     }
 
 }
+
+const Styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor : '#1B4F72'
+    }
+})
 
 export default Soup;
